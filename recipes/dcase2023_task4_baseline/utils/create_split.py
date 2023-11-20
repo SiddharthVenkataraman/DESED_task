c0e1c2f7711e79f01f3ced2f4ecae65754fd4826
Malchemis <eliasboulanger1@gmail.com>
2023-11-20 17:19:25 +0100

    Update file paths and move files for each set

--- recipes/dcase2023_task4_baseline/utils/create_split.py
+++ recipes/dcase2023_task4_baseline/utils/create_split.py
@@ -45,12 +45,14 @@ def split_df(dataframe: pd.DataFrame, dur_df: pd.DataFrame, split_ratios: dict, 
 
     # Function to update paths and move files
     def update_paths_and_move_files(df, set_name):
-        if not os.path.exists(set_name):
-            os.makedirs(set_name)
+        new_folder = os.path.join(base_folder, set_name)
+        if not os.path.exists(new_folder):
+            os.makedirs(new_folder)
         df['filename'] = df['filename'].apply(lambda x: os.path.join(set_name, os.path.basename(x)))
         for file in df['filename']:
             src = os.path.join(base_folder, os.path.basename(file))
-            shutil.copy(src, file)
+            dst = os.path.join(new_folder, os.path.basename(file))
+            shutil.copy(src, dst)
 
     # Update paths and move files for each set
     for set_name, set_df in zip(['strong', 'synth_train', 'synth_val', 'weak', 'unlabeled', 'test'], [strong, synth_train, synth_val, weak, unlabeled, test]):
