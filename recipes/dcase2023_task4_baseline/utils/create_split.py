fe19432b026e9699da1317a6d90fc54c17fc8095
Malchemis <eliasboulanger1@gmail.com>
2023-11-20 15:57:21 +0100

    Refactor split_df function to handle multiple sets and move files

--- recipes/dcase2023_task4_baseline/utils/create_split.py
+++ recipes/dcase2023_task4_baseline/utils/create_split.py
@@ -1,53 +1,81 @@
-"""Takes a pandas datadrame and split it according to the split ratio [train, test]"""
-
-from sklearn.model_selection import train_test_split
+import os
+import shutil
 import pandas as pd
 import argparse
+from sklearn.model_selection import train_test_split
 
-def split_df(df: pd.DataFrame, dur_df:pd.DataFrame, split_ratio: list, random_state: int = 42):
-    """Splits a dataframe into train, test and val according to the split ratio [train, test]
-    Args:
-        df (pd.DataFrame): Dataframe to split
-        dur_df (pd.DataFrame): Dataframe with the duration of each file
-        split_ratio (list): Ratio to split the dataframe
-        random_state (int, optional): Random state for the split. Defaults to 42.
-    Returns:
-        list: List of dataframes [train, test]
+def split_df(df: pd.DataFrame, dur_df: pd.DataFrame, split_ratios: dict, base_folder: str, random_state: int = 42):
     """
-    csv = df.copy()
-    csv.filename = csv.filename.apply(lambda x: x.split("/")[-1])
-    print(f"Total: {len(csv)} files")
-    train, test = train_test_split(csv, test_size=split_ratio[1], random_state=random_state)
-    print(f"Train: {len(train)} files, Test: {len(test)} files")
-    train_dur = dur_df[dur_df.filename.isin(train.filename)]
-    test_dur = dur_df[dur_df.filename.isin(test.filename)]
-    print(f"Train duration: {train_dur.duration.sum()} s, Test duration: {test_dur.duration.sum()} s")
-    return [(train, train_dur), (test, test_dur)]
+    Splits a dataframe into sets and moves files into corresponding folders.
+    Args:
+        df (pd.DataFrame): Dataframe to split.
+        dur_df (pd.DataFrame): Dataframe with the duration of each file.
+        split_ratios (dict): Ratios to split the dataframe.
+        base_folder (str): Base folder where the original files are located.
+        random_state (int): Random state for the split. Defaults to 42.
+    Returns:
+        dict: Dictionary of tuples (dataframe, duration dataframe) for each set.
+    """
+    # Initial split to separate Test Set
+    remaining, test = train_test_split(df, test_size=split_ratios['test'], random_state=random_state)
 
- 
+    # Further splitting for other sets
+    proportions = {key: val / (1 - split_ratios['test']) for key, val in split_ratios.items() if key != 'test'}
+    strong, remaining = train_test_split(remaining, test_size=1-proportions['strong'], random_state=random_state)
+    synth, remaining = train_test_split(remaining, test_size=1-proportions['synth']/(1-proportions['strong']), random_state=random_state)
+    weak, unlabeled = train_test_split(remaining, test_size=1-proportions['weak']/(1-proportions['synth']/(1-proportions['strong'])), random_state=random_state)
+
+    # Function to update paths and move files
+    def update_paths_and_move_files(df, set_name):
+        new_folder = os.path.join(base_folder, set_name)
+        if not os.path.exists(new_folder):
+            os.makedirs(new_folder)
+        df['filename'] = df['filename'].apply(lambda x: os.path.join(set_name, os.path.basename(x)))
+        for file in df['filename']:
+            src = os.path.join(base_folder, os.path.basename(file))
+            dst = os.path.join(base_folder, file)
+            shutil.move(src, dst)
+
+    # Update paths and move files for each set
+    for set_name, set_df in zip(['strong', 'synth', 'weak', 'unlabeled', 'test'], [strong, synth, weak, unlabeled, test]):
+        update_paths_and_move_files(set_df, set_name)
+
+    # Creating duration dataframes for each set
+    strong_dur = dur_df[dur_df.filename.isin(strong.filename)]
+    synth_dur = dur_df[dur_df.filename.isin(synth.filename)]
+    weak_dur = dur_df[dur_df.filename.isin(weak.filename)]
+    unlabeled_dur = dur_df[dur_df.filename.isin(unlabeled.filename)]
+    test_dur = dur_df[dur_df.filename.isin(test.filename)]
+
+    return {
+        'strong': (strong, strong_dur),
+        'synth': (synth, synth_dur),
+        'weak': (weak, weak_dur),
+        'unlabeled': (unlabeled, unlabeled_dur),
+        'test': (test, test_dur)
+    }
+
 def main(args):
     df = pd.read_csv(args.input_tsv, sep="\t")
     dur_df = pd.read_csv(args.input_dur, sep="\t")
-    
-    # Split dataframe
-    split_dfs = split_df(df, dur_df, args.split_ratio)
-    outputs = [args.output_train, args.output_test]
-    for i, split_tsv in enumerate(split_dfs):
-        split_tsv[0].to_csv(outputs[i], sep="\t", index=False)
-        split_tsv[1].to_csv(outputs[i].replace(".tsv", "_duration.tsv"), sep="\t", index=False)
-        print(f"Saved {outputs[i]}")
-    
-    # Create empty dataframe for placeholder
-    placeholder = pd.DataFrame(columns=df.columns)
-    placeholder.to_csv("placeholder.tsv", sep="\t", index=False)
-        
+
+    # Split dataframe and move files
+    split_dfs = split_df(df, dur_df, args.split_ratios, args.base_folder)
+
+    # Save split dataframes
+    for set_name, (data_df, dur_df) in split_dfs.items():
+        output_file = f"{set_name}.tsv"
+        duration_file = f"{set_name}_duration.tsv"
+        data_df.to_csv(output_file, sep="\t", index=False)
+        dur_df.to_csv(duration_file, sep="\t", index=False)
+        print(f"Saved {output_file} and {duration_file}")
+
 if __name__ == '__main__':
-    print("Splitting dataframe")
-    parser = argparse.ArgumentParser(description="Split a dataframe into train, test and val according to the split ratio [train, test] (val is the rest)")
-    parser.add_argument("--i_tsv", dest="input_tsv", type=str, required=True, help="Path to the input tsv file")
-    parser.add_argument("--i_dur", dest="input_dur", type=str, required=True, help="Path to the input duration tsv file")
-    parser.add_argument("--o_train", dest="output_train", type=str, required=False, help="Path to the output train tsv file", default="train.tsv")
-    parser.add_argument("--o_test", dest="output_test", type=str, required=False, help="Path to the output test tsv file", default="test.tsv")
-    parser.add_argument("--split_ratio", dest="split_ratio", type=float, nargs=2, required=False, help="Ratio to split the dataframe", default=[0.8, 0.2])
+    print("Splitting dataframe and moving files")
+    parser = argparse.ArgumentParser(description="Split a dataframe into sets and distribute files into corresponding folders.")
+    parser.add_argument("--input_tsv", type=str, required=True, help="Path to the input tsv file")
+    parser.add_argument("--input_dur", type=str, required=True, help="Path to the input duration tsv file")
+    parser.add_argument("--base_folder", type=str, required=True, help="Base folder where the original files are located")
+    parser.add_argument("--split_ratios", type=float, nargs=5, required=True, help="Ratios to split the dataframe (strong, synth, weak, unlabeled, test)")
     args = parser.parse_args()
     main(args)
