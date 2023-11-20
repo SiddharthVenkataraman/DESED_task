8668a88c3b9867ba1d161508e53a7a05703cc6ca
Malchemis <eliasboulanger1@gmail.com>
2023-11-20 17:48:48 +0100

    Update classes_dict.py with new class labels

--- recipes/dcase2023_task4_baseline/local/classes_dict.py
+++ recipes/dcase2023_task4_baseline/local/classes_dict.py
@@ -7,16 +7,7 @@ from collections import OrderedDict
 
 classes_labels = OrderedDict(
     {
-        "Alarm_bell_ringing": 0,
-        "Blender": 1,
-        "Cat": 2,
-        "Dishes": 3,
-        "Dog": 4,
-        "Electric_shaver_toothbrush": 5,
-        "Frying": 6,
-        "Running_water": 7,
-        "Speech": 8,
-        "Vacuum_cleaner": 9,
-        "Vehicle_single":10,
+        "car": 0,
+        "lorry": 1,
     }
 )
