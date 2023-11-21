bc12bf266d434622ab5e9caada13edbc2d310ffd
Malchemis <eliasboulanger1@gmail.com>
2023-11-21 13:56:25 +0100

    Add print statements to encapsulate_run.py

--- recipes/dcase2023_task4_baseline/encapsulate_run.py
+++ recipes/dcase2023_task4_baseline/encapsulate_run.py
@@ -7,6 +7,8 @@ import yaml
 
 def main(command="python3 train_sed.py --conf_file confs/default_SV.yaml --gpus 1"):
     try:
+        
+        print(f'Loading the config file from {command}')
         # Parse the command and load the config file
         command = command.split(" ")
         config_file = command[command.index("--conf_file")+1]
@@ -15,6 +17,7 @@ def main(command="python3 train_sed.py --conf_file confs/default_SV.yaml --gpus 
         
         # Get the parameters from nni
         params = nni.get_next_parameter()
+        print(f"Parameters from nni: {params}")
         
         # Update the config file with the new parameters
         for key, value in params.items():
@@ -29,12 +32,17 @@ def main(command="python3 train_sed.py --conf_file confs/default_SV.yaml --gpus 
         with open("config_nni.yaml", "w") as f:
             yaml.dump(config, f)
             
+        print("New config file saved as 'config_nni.yaml'")
+            
         # Update the command
         command[command.index("--conf_file")+1] = "config_nni.yaml"
         command = " ".join(command)                    
         os.system(command) # training run
-    finally:
-        os.system(f"python3 utils/clean_process.py --target_command '{command}'") # kill the process
+    
+    except Exception as e:
+        print(f"Error: {e}")    
+    
+    os.system(f"python3 utils/clean_process.py --target_command '{command}'") # kill the process
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
